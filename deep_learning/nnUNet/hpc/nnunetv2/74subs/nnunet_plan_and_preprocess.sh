e0c901bb17911b9963186ddfaea3f5fd637e95d5
jaimebarran <jaimebarran@gmail.com>
2024-07-17 16:51:42 +0200

    add jobfiles 74subs

--- deep_learning/nnUNet/hpc/nnunetv2/74subs/nnunet_plan_and_preprocess.sh
+++ deep_learning/nnUNet/hpc/nnunetv2/74subs/nnunet_plan_and_preprocess.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-#SBATCH --job-name=nn_bc
-#SBATCH --chdir=/cluster/home/ja4922/AEye/nnunetv2/35subs
+#SBATCH --job-name=nn_pp
+#SBATCH --chdir=/cluster/home/ja4922/AEye/nnunetv2/74subs
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jaime.barranco-hernandez@chuv.ch
 #SBATCH --ntasks=1
 #SBATCH --time=1-23:59:00
-#SBATCH --output=bc_%N_%j_%a.out
-#SBATCH --error=bc_%N_%j_%a.err
+#SBATCH --output=pp_%N_%j_%a.out
+#SBATCH --error=pp_%N_%j_%a.err
 #SBATCH --cpus-per-task=10
 #SBATCH --mem=64gb
 #SBATCH --account rad
@@ -16,4 +16,4 @@
 export SINGULARITY_TMPDIR=/data/bach/Jaime/singularity/tmp
 export SINGULARITY_CACHEDIR=/data/bach/Jaime/singularity/cache
 
-singularity run --bind /data/bach/Jaime/nnunet_guillaume/nnUNet_35subs:/opt/nnunet_resources --nv docker://jaimebarran/nnunet:0.1.0 nnUNetv2_find_best_configuration 313 -c 2d 3d_fullres
+singularity run --bind /data/bach/Jaime/nnunet_guillaume/nnUNet_74subs:/opt/nnunet_resources --nv docker://jaimebarran/nnunet:0.1.0 nnUNetv2_plan_and_preprocess -d 074 -c 3d_fullres --verify_dataset_integrity
--- deep_learning/nnUNet/hpc/nnunetv2/74subs/nnunet_train_f3.sh
+++ deep_learning/nnUNet/hpc/nnunetv2/74subs/nnunet_train_f3.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
-#SBATCH --job-name=nn_bc
+#SBATCH --job-name=nn_f3
 #SBATCH --chdir=/cluster/home/ja4922/AEye/nnunetv2/35subs
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jaime.barranco-hernandez@chuv.ch
 #SBATCH --ntasks=1
 #SBATCH --time=1-23:59:00
-#SBATCH --output=bc_%N_%j_%a.out
-#SBATCH --error=bc_%N_%j_%a.err
+#SBATCH --output=f3_%N_%j_%a.out
+#SBATCH --error=f3_%N_%j_%a.err
 #SBATCH --cpus-per-task=10
 #SBATCH --mem=64gb
 #SBATCH --account rad
 #SBATCH --partition rad
-#SBATCH --gres=gpu:rtx2080:1
+#SBATCH --gres=gpu:rtx3090:1
 
 export SINGULARITY_TMPDIR=/data/bach/Jaime/singularity/tmp
 export SINGULARITY_CACHEDIR=/data/bach/Jaime/singularity/cache
+export nnUNet_compile=f
 
-singularity run --bind /data/bach/Jaime/nnunet_guillaume/nnUNet_35subs:/opt/nnunet_resources --nv docker://jaimebarran/nnunet:0.1.0 nnUNetv2_find_best_configuration 313 -c 2d 3d_fullres
+singularity run --bind /data/bach/Jaime/nnUNet_guillaume:/opt/nnunet_resources --nv docker://jaimebarran/nnunet:0.1.0 nnUNetv2_train 313 3d_fullres 3 --npz
